47b7b98519ddce2c8e403de7d4ce8059fe629519
Ahmed Abbas <ahmed.abbas@nos.nl>
2024-07-07 19:55:56 +0200

    feat: add react router

--- src/app.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div>oh hello world to you</div>;
-}
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app.tsx";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Root } from "@/routes/root.tsx";
+import { Home } from "@/views/home.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
