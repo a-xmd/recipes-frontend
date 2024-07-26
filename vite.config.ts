b449191c2bc51eca518bc7d36f8afc422aa74209
Ahmed Abbas <ahmed.abbas@nos.nl>
2024-07-26 14:22:53 +0200

    feat: update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 import path from "node:path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svgr(), react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig((mode) => {
+  return {
+    plugins: [svgr(), react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  server: {
-    proxy: {
-      "/api": "http://localhost:3000",
+    server: {
+      proxy: {
+        "/api": loadEnv("all", process.cwd()).VITE_API_URL,
+      },
     },
-  },
+  };
 });
