2a5ef26ea824c7e75f6bf0dfb9dab63c32969898
Ahmed Abbas <ahmed.abbas@nos.nl>
2024-07-28 11:59:59 +0200

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 import path from "node:path";
@@ -10,6 +10,11 @@ export default defineConfig(() => {
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    server: {
+      proxy: {
+        "/api": "http://localhost:5000",
       },
     },
   };
