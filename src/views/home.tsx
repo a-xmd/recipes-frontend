ed74e8f9a1511ee1a6864859b3dddee337471ab8
Ahmed Abbas <ahmed.abbas@nos.nl>
2024-07-28 12:14:13 +0200

    wip

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,3 +1,17 @@
+import { createApiUrl } from "@/utils/url.ts";
+
 export const Home = () => {
-  return <div>view for home</div>;
+  return (
+    <div>
+      <div>view for home</div>
+      <button
+        onClick={async () => {
+          const res = await fetch(createApiUrl("/api/views"));
+          console.log(await res.json());
+        }}
+      >
+        click me
+      </button>
+    </div>
+  );
 };
